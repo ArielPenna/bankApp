e31050c84564c68538a4590a853b28a46a9399cf
Matias Ruiz <matimalcomcarp@gmail.com>
2020-09-10 14:57:51 -0300

    User mayores de 16 años

--- api/src/models/user.js
+++ api/src/models/user.js
@@ -1,44 +1,53 @@
-const S = require('sequelize').DataTypes 
+const S = require('sequelize').DataTypes
 
 module.exports = (sequelize) => {
-  sequelize.define( 'user',
-    {
-      firstName: {
-        type: S.STRING,
-        allowNull: false,
-      },
-      lastName: {
-        type: S.STRING,
-        allowNull: false,
-      },
-      email: {
-        type: S.STRING,
-        allowNull: false
-      },
-      documentType: {
-        type: S.ENUM('DNI', 'Pasaporte'),
-        allowNull: false
-      },
-      documentNumber: {
-        type: S.INTEGER,
-        allowNull: false
-      },
-      birth: {
-        type: S.DATE,
-        allowNull: false
-      },
-      phoneNumber: {
-        type: S.TEXT,// S.INTEGER(11).UNSIGNED | numero de telefono sin +54
-        allowNull: false
-      },
-      password: {
-        type: S.STRING,
-        allowNull: false,
-      },
-      access: {
-        type: S.ENUM('User', 'Admin'),
-        defaultValue: 'User'
-      }
-    },
-  );
+    sequelize.define('user', {
+        firstName: {
+            type: S.STRING,
+            allowNull: false,
+        },
+        lastName: {
+            type: S.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: S.STRING,
+            allowNull: false
+        },
+        documentType: {
+            type: S.ENUM('DNI', 'Pasaporte'),
+            allowNull: false
+        },
+        documentNumber: {
+            type: S.INTEGER,
+            allowNull: false
+        },
+        birthDate: {
+            type: S.DATEONLY,
+            allowNull: true,
+            validate: {
+                isDate: true,
+                dateValidator(value) {
+                    let ageCheck = new Date();
+                    ageCheck.setFullYear(ageCheck.getFullYear() - 16);
+                    let birthDate = new Date(value);
+                    if (ageCheck < birthDate) {
+                        throw new Error("No esta permitido registrar a Usuarios menores de 16 años");
+                    }
+                }
+            },
+        },
+        phoneNumber: {
+            type: S.TEXT, // S.INTEGER(11).UNSIGNED | numero de telefono sin +54
+            allowNull: false
+        },
+        password: {
+            type: S.STRING,
+            allowNull: false,
+        },
+        access: {
+            type: S.ENUM('User', 'Admin'),
+            defaultValue: 'User'
+        }
+    }, );
 };
