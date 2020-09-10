71ef4c95c1269ac05b20910f931cac7d64dca60c
Matias Ruiz <matimalcomcarp@gmail.com>
2020-09-10 19:03:26 -0300

    routes user

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -20,26 +20,26 @@ const router = Router();
 router.use('/home', () => {})
 
 //invalidar rutas, es solo de prueba
-router.get('/create/account',(req, res) => {
+router.get('/create/account', (req, res) => {
     Account.create()
         .then(
-            account => res.send(account), 
+            account => res.send(account),
             err => res.send(err)
         )
 })
-router.get('/create/user',(req, res) => {
+router.get('/create/user', (req, res) => {
     Account.create({
-        firstName: 'tomas',
-        lastName: 'vasquez',
-        email: 'tomasbenjamin117@gmail.com',
-        documentType: 'DNI',
-        documentNumber: 43598434,
-        phoneNumber: 01125336221,
-        password: 'asdasd',
-        address: 'tu vieja 123'
-    })
+            firstName: 'tomas',
+            lastName: 'vasquez',
+            email: 'tomasbenjamin117@gmail.com',
+            documentType: 'DNI',
+            documentNumber: 43598434,
+            phoneNumber: 01125336221,
+            address: 'tu vieja 123',
+            password: 'asdasd',
+        })
         .then(
-            account => res.send(account), 
+            account => res.send(account),
             err => res.send(err)
         )
 })
