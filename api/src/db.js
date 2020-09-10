86a62961f2ef9da1ead545b5482fa569ed1c0069
tomas0011 <tomasbenjamin117@gmail.com>
2020-09-10 00:10:59 -0300

    creacion de build de api, creacion de modelos de tablas(users, wallets), creacion de relacion 1a1 entre tablas

--- api/src/db.js
+++ api/src/db.js
@@ -1,4 +1,7 @@
+require("dotenv").config();
 const { Sequelize } = require('sequelize');
+const fs = require("fs");
+const path = require("path");
 const { DB_USER, DB_PASSWORD, DB_HOST,} = process.env;
 
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/bankapp`, {
@@ -6,6 +9,49 @@ const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
 
+const basename = path.basename(__filename);
+
+const modelDefiners = [];
+
+// Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
+fs.readdirSync(path.join(__dirname, "/models"))
+  .filter(
+    (file) =>
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+  )
+  .forEach((file) => {
+    modelDefiners.push(require(path.join(__dirname, "/models", file)));
+  });
+
+// Inyectamos la conexión (sequelize) a todos los modelos
+modelDefiners.forEach((model) => model(sequelize));
+// Capitalizamos los nombres de los modelos ie: product => Product
+let entries = Object.entries(sequelize.models);
+let capsEntries = entries.map((entry) => [
+  entry[0][0].toUpperCase() + entry[0].slice(1),
+  entry[1],
+]);
+sequelize.models = Object.fromEntries(capsEntries);
+
+// En sequelize.models están todos los modelos importados como propiedades
+// Para relacionarlos hacemos un destructuring
+const { User, Wallet } = sequelize.models;
+
+//-----------------------^^^^^^^^^^^^^^^^^^^^^^-------------------------------//
+//-----------------------||||||||||||||||||||||-------------------------------//
+//-----------------------||--DECLARACIONES--|||-------------------------------//
+//-----------------------||||||||||||||||||||||-------------------------------//
+//-----------------------|||--RELACIONES--|||||-------------------------------//
+//-----------------------||||||||||||||||||||||-------------------------------//
+//-----------------------vvvvvvvvvvvvvvvvvvvvvv-------------------------------//
+
+// un usuario tiene una billetera
+// una billetera tiene un usuario
+User.hasOne(Wallet) // busca userId en wallet
+Wallet.belongsTo(User) // crea userId
+
+
+//----------------------------------------------------------------------------//
 module.exports = {
     ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
     conn: sequelize,     // para importart la conexión { conn } = require('./db.js');
