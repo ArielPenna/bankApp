9189724cad81e797093c5dac17a0da6bf93c88ec
tomas0011 <tomasbenjamin117@gmail.com>
2020-09-12 02:52:17 -0300

    transacciones andando pero desprolijo

--- api/src/routes/path/transactions.js
+++ api/src/routes/path/transactions.js
@@ -2,25 +2,36 @@ const server = require("express").Router();
 /* const bcrypt = require("bcryptjs"); */
 const { User, Wallet } = require("../../db.js");
 
-server.post("/:idFrom/:idTo", async (req, res) => {
+server.post("/:idFrom/:idTo", (req, res) => {
     const { idFrom, idTo } = req.params
     const { transaction } = req.body
-    console.log(idFrom)
-    console.log(idTo)
-    console.log('antes del find')
 
-    let from = await User.findByPk(idFrom)
-    let to = await User.findByPk(idTo)
-    if(to && from && transaction){
-        let fromWallet = await from.getWallet()
-        let toWallet = await to.getWallet()
-        console.log('fromWallet ', fromWallet.dataValues)
-        console.log('toWallet ',toWallet.dataValues)
-        await fromWallet.update({ balance: (fromWallet.dataValues.balance - transaction) })
-        await toWallet.update({ balance: (toWallet.dataValues.balance + transaction) })
-        res.send('transaccion realizada con exito')
-    }
-    res.send('chau')
+
+    User.findByPk(idFrom).then(
+        from => from.getWallet().then(
+            (wallet)=>{
+                console.log('wallet from ',wallet)
+                wallet.update({
+                    balance:(parseFloat(wallet.dataValues.balance) - parseFloat(transaction))
+                })
+                    .then(up => console.log(up.dataValues))
+                    .catch(err => res.send('ocurrio un error en el update de la wallet'))
+            }
+        ).catch(err => res.send('ocurrio un error en la Wallet de from'))
+    ).catch(err => res.send('ocurrio un error en from'))
+
+    User.findByPk(idTo).then(
+        to => to.getWallet().then(
+            (wallet)=>{
+                console.log('wallet to ',wallet.dataValues)
+                wallet.update({
+                    balance:(parseFloat(wallet.dataValues.balance) + parseFloat(transaction))
+                })
+                    .then(up => res.send(up.dataValues))
+                    .catch(err => res.send('ocurrio un error en el update de la wallet de to'))
+            }
+        ).catch(err => res.send('ocurrio un error en la Wallet de to'))
+    ).catch(err => res.send('ocurrio un error en to'))
 })
 
 module.exports = server;
