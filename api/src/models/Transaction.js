7828148a24a282ebc28169af1103c70c171c9e01
ArielPenna <arielpenna@gmail.com>
2020-09-14 15:40:03 -0300

    nuevo estilo visual, con algunos ajustes en componentes y Header nuevo

--- api/src/models/Transaction.js
+++ api/src/models/Transaction.js
@@ -1,21 +1,14 @@
-const S = require('sequelize').DataTypes 
+const S = require("sequelize").DataTypes;
 
 module.exports = (sequelize) => {
-    sequelize.define( 'transaction',
-        {
-            value: {
-                type: S.DECIMAL(1000, 2),
-                allowNull: false
-            },
-            transactions_type: {
-                type: S.ENUM([
-                    "transferencia a usuario",
-                    "pago comercio",
-                    "transferencia bancaria",
-                    "recarga billetera",
-                ]),
-                allowNull: true,
-            },
-        },
-    );
+  sequelize.define("transaction", {
+    value: {
+      type: S.DECIMAL(1000, 2),
+      allowNull: false,
+    },
+    transactions_type: {
+      type: S.ENUM(["transferencia a usuario", "pago comercio", "transferencia bancaria", "recarga billetera"]),
+      allowNull: true,
+    },
+  });
 };
