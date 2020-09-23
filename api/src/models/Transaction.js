bf134733ac2c0f3c05a8d1337d406a19c22b5d72
tomas0011 <tomasbenjamin117@gmail.com>
2020-09-23 15:22:02 -0300

    ticket de transaccion, numero en contacto

--- api/src/models/Transaction.js
+++ api/src/models/Transaction.js
@@ -1,33 +1,42 @@
 const S = require("sequelize").DataTypes;
 
+let value = 4572
+
 module.exports = (sequelize) => {
-    sequelize.define( 'transaction',
-        {
-            id: {
-                type: S.INTEGER,
-                autoIncrement: true,
-                primaryKey: true
-            },
-            debit: {
-                type: S.TEXT
-            },
-            deposit: {
-                type: S.TEXT
-            },
-            value: {
-                type: S.DECIMAL(1000, 2),
-                defaultValue: 0.0,
-                validate: { min: 0 }
-            },
-            transactions_type: {
-                type: S.ENUM([
-                    "transferencia a usuario",
-                    "pago comercio",
-                    "transferencia bancaria",
-                    "recarga billetera",
-                ]),
-                defaultValue: "transferencia a usuario"
-            },
+    sequelize.define( 'transaction', {
+        id: {
+            type: S.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        ticket: {
+            type: S.INTEGER,
+            unique: true
+        },
+        debit: {
+            type: S.TEXT
+        },
+        deposit: {
+            type: S.TEXT
+        },
+        value: {
+            type: S.DECIMAL(1000, 2),
+            defaultValue: 0.0,
+            validate: { min: 0 }
+        },
+        transactions_type: {
+            type: S.ENUM([
+                "transferencia a usuario",
+                "pago comercio",
+                "transferencia bancaria",
+                "recarga billetera",
+            ]),
+            defaultValue: "transferencia a usuario"
+        },
+    }, {
+        hooks: {
+            beforeCreate: (transaction) => transaction.dataValues.ticket = value++
         }
-    );
+    });
 };
+
