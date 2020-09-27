7406997ef72c86363aeb8c6d966bf6b6e08eabac
ArielPenna <arielpenna@gmail.com>
2020-09-26 21:57:08 -0300

    RECHARGE UX

--- app/components/screens/money/styles/RechargeStyles.jsx
+++ app/components/screens/money/styles/RechargeStyles.jsx
@@ -1,75 +1,27 @@
 import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
-   container:{
-      flex:1,   
-      width: "100%",
-      height: "100%",
-      alignContent:"center",    
-      justifyContent: "center",
-      alignItems: "center",  
-   },
+   container:
+      { flex: 1, width: "100%", height: "100%", alignContent: "center", justifyContent: "center", alignItems: "center" },
 
-   title:{
-      marginLeft:75,
-      marginTop:30,
-      color:"#fff",
-      width:200,
-      fontSize:25,
-      textAlign:"center",      
-   },
+   containerInput:
+      { backgroundColor: "#f7b700", width: 340, height: 444, borderRadius: 2, shadowOpacity: 0.57, shadowRadius: 15.19, elevation: 8 },
 
-   containerInput:{
-      backgroundColor:"#ECE8AF",
-      width:340,
-      height:444,
-      borderRadius:15,
-      shadowOpacity: 0.57,
-      shadowRadius: 15.19,
-      elevation: 23,
-   },
-
-   text:{
-      fontSize:25,
-      padding:22,
-   },
+   text:
+      { fontSize: 20, padding: 22 },
    
-   inputMoney:{
-      width:296,
-      height:55,
-      marginLeft: 17,
-      marginTop:25,
-      borderRadius: 15,
-      backgroundColor:"#8F958B",
-      fontSize: 20,
-      textAlign: 'center',
-   },
+   text2:
+      { paddingLeft: 25, paddingRight: 15, paddingTop: 25, fontSize: 20, marginBottom: 58 },
    
-   text2:{
-      paddingLeft:25,
-      paddingRight:15,
-      paddingTop:25,
-      fontSize:25,
-      marginBottom:58
-   },
+   inputMoney:
+      { width: 296, height: 55, marginLeft: 17, marginTop: 25, borderRadius: 2, backgroundColor: "white", fontSize: 20, textAlign: 'center' },
+   
 
-   btn:{
-      width: '90%',
-      marginLeft: '20px',
-      marginBottom: '10px',
-      
-      marginTop: -23,
-      backgroundColor: "#009688",
-      borderRadius: 5,
-      paddingVertical: 10,
-      paddingHorizontal: 12
-   },
+   btn:
+      { width: '90%', marginLeft: '20px', marginBottom: '10px', marginTop: -23, backgroundColor: "#752667", borderRadius: 5, paddingVertical: 10, paddingHorizontal: 12 },
 
-   appButtonText:{
-      color:"#fff",
-      fontSize:25,
-      textAlign:"center"
-   }
+   appButtonText:
+      { color: "#f7b700", fontSize: 22, textAlign: "center" }
 })
 
 export default styles
--- app/components/screens/money/styles/ValidateStyles.jsx
+++ app/components/screens/money/styles/ValidateStyles.jsx
@@ -2,61 +2,26 @@ import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
 
-   container:{
-      flex:1,   
-      width: "100%",
-      height: "100%",
-      alignContent:"center",    
-      justifyContent: "center",
-      alignItems: "center",
-   },
-   title:{
-      color:"#fff",
-      width:200,
-      fontSize:25,
-      textAlign:"center",
-      
-   },
-   container2:{
-      backgroundColor:"#ECE8AF",
-      width:340,
-      height:520,
-      borderRadius:15,
-      shadowOpacity: 0.57,
-      shadowRadius: 15.19,
-      elevation: 23,
-   },
+   container:
+      { flex: 1, width: "100%", height: "100%", alignContent: "center", justifyContent: "center", alignItems: "center" },
 
-   text:{
-      flex:1,
-      fontSize:25,
-      padding:20,
-   },
+   container2:
+      { backgroundColor: "#f7b700", width: 340, height: 444, borderRadius: 2, shadowOpacity: 0.57, shadowRadius: 15.19, elevation: 8 },
 
-   numValidation:{
-      flex:1,
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      backgroundColor: "black",
-      color: "#fff",
-      fontSize: 45,
-   },
-   btn:{
-      width: '90%',
-      marginLeft: '20px',
-      marginBottom: '10px',
-      marginTop: -23,
-      backgroundColor: "#009688",
-      borderRadius: 5,
-      paddingVertical: 10,
-      paddingHorizontal: 12
-   },
-   appButtonText:{
-      color:"#fff",
-      fontSize:25,
-      textAlign:"center"
-   }
+   text:
+      { fontSize: 20, padding: 22 },
+   
+   text2:
+      { paddingLeft: 25, paddingRight: 15, paddingTop: 25, fontSize: 20, marginBottom: 58 },
+
+   numValidation:
+      { flex: 1, display: "flex", justifyContent: "center", alignItems: "center", backgroundColor: "black", color: "#fff", fontSize: 45 },
+
+   btn:
+      { width: '90%', marginLeft: '20px', marginBottom: '10px', marginTop: -23, backgroundColor: "#752667", borderRadius: 5, paddingVertical: 10, paddingHorizontal: 12 },
+
+   appButtonText:
+      { color: "#f7b700", fontSize: 22, textAlign: "center" }
 
 })
 
